2b1f3b1f6e1747bc05e8e987d589b0c86f0a8c83
Tom <tom@vycital.eu>
2026-04-26 13:55:59 +0200

    feat(redirects): rename inbox to file-drop with new link
    The inbox alias was unclear; file-drop better describes the route's
    purpose. Also points to a fresh share URL since the old one is no
    longer in use.

---
+++ src/routes/(redirect)/file-drop/+page.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+  <title>https://murena.io/s/C93fCM4rAe76mxD</title>
+  <meta
+    http-equiv="refresh"
+    content="0; url=https://murena.io/s/C93fCM4rAe76mxD"
+  />
+</svelte:head>
+
+<a href="https://murena.io/s/C93fCM4rAe76mxD"
+  >https://murena.io/s/C93fCM4rAe76mxD</a
+>
